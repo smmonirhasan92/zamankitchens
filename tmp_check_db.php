a84acb78368589818569713dc222418a9a5be97b
smmonirhasan92 <smmonirhasan92@gmail.com>
2026-03-28 18:19:59 +0600

    Cleanup temporary diagnostics script

--- tmp_check_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'includes/db.php';
-$tables = ['products', 'categories'];
-foreach($tables as $table) {
-    echo "--- TABLE: $table ---\n";
-    $stmt = $pdo->query("SHOW CREATE TABLE $table");
-    echo $stmt->fetchColumn() . "\n\n";
-}
